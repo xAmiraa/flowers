918e1cf4adae949ab66e546d39c20e9a1cca82a7
amira <amiramahana.x96@gmail.com>
2021-05-24 17:34:00 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express=require('express')
+const app=express()
+const path =require('path')
+app.use(express.static(__dirname + "/dist/flowers"))
+app.listen(process.env.PORT || 3000)
+app.get ('/*',(req ,res)=>{
+res .sendFile(path.join(__dirname + 'dist/pet-shop/index.html'))
+})
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
